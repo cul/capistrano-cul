34c4b49e39ffd599890857bae3b85e21ee07c471
Carla Galarza <cmg2228@columbia.edu>
2017-08-03 16:05:50 -0400

    Generated gem using bundler.

---
+++ lib/capistrano/cul.rb
@@ -0,0 +1,7 @@
+require "capistrano/cul/version"
+
+module Capistrano
+  module Cul
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/cul/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Cul
+    VERSION = "0.1.0"
+  end
+end
